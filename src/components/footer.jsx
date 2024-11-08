a6546931866f98c2c4fc7f3a9d60d91263a1adba
mvoidets <mvoidets@yahoo.com>
2024-11-08 11:45:11 -0700

    changed path to svg in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,10 +3,10 @@ const Footer = () => {
     <footer className="footer mt-auto py-3 bg-transparent fixed-bottom">
       <div className=" d-flex justify-content-center align-items-center">
         <a href="https://github.com/mvoidets" target="_blank" rel="noopener noreferrer" className="mx-2">
-          <img src="../src/assets/github-mark.png"alt="GitHub" width={50} height={50} />
+          <img src="/github-mark.png"alt="GitHub" width={50} height={50} />
         </a>
         <a href="https://www.linkedin.com/in/michelle-voidets-98360194/" target="_blank" rel="noopener noreferrer" className="mx-2">
-          <img src="../src/assets/linkedin.png" alt="LinkedIn" width={50} height={50} />
+          <img src="/linkedin.png" alt="LinkedIn" width={50} height={50} />
         </a>
         
         <a href="https://stackoverflow.com/users/28201915/mvoid" target="_blank" rel="noopener noreferrer" className="mx-2">
